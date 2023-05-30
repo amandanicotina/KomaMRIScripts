15f87524f217083b6c87f867a5ab313714bfd6c6
Amanda Nicotina <amandanicotina@gmail.com>
2023-05-30 10:33:51 +0200

    new folder to try better script

---
+++ MainFiles/main.jl
@@ -0,0 +1,28 @@
+using KomaMRI
+
+include("shaped_pulse.jl")
+include("phantom.jl")
+include("magnetization.jl")
+
+# seq
+    # matlab files
+    
+# phantom
+    # T1, T2, ΔB0
+    # postion
+    # name
+
+# magnetization
+    # seq
+    # phantom
+    # pieces
+
+# fidelity?
+    # magnetization
+    # matlab magnetization
+
+# plot comparisons, max, min, etc. 
+
+
+
+
